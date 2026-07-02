32c1147aeb298375d40adefddd622c2db3f67c56
bazinge <jasonadon@gmail.com>
2026-07-02 22:18:33 +1000

    change instance type to t3.micro

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
   ami           = "ami-003e7cf4a21db7053"
-  instance_type = "t3.nano"  # adjust as needed
+  instance_type = "t3.micro"  # adjust as needed
 
   tags = {
     Name = "Windows-EKS-Optimized-Server"
