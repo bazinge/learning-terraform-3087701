e3fdb786a24c6d7882de357f3b1a92e62132fff8
bazinge <jasonadon@gmail.com>
2026-07-02 22:03:29 +1000

    update main

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["Windows_Server-2025-English-Core-EKS_Optimized-1.35-2026.04.18"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["687402702948"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+resource "aws_instance" "app" {
+  ami           = "ami-003e7cf4a21db7053"
+  instance_type = "t3.medium"  # adjust as needed
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Windows-EKS-Optimized-Server"
   }
 }
