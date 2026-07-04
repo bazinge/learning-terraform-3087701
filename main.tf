d18fe2b8a7a64ef097acf71abe9ea55f1113ad74
bazinge <jasonadon@gmail.com>
2026-07-04 21:00:55 +1000

    update instance type

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "app" {
+resource "aws_instance" "ubuntu_server" {
   #ami           = "ami-003e7cf4a21db7053"
   ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"  # adjust as needed
