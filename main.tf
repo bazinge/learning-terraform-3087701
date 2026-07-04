9eba84c179db3a22102de0793ccde029d637a783
bazinge <jasonadon@gmail.com>
2026-07-04 20:56:06 +1000

    deploy Ubuntu

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "app" {
-  ami           = "ami-003e7cf4a21db7053"
+  #ami           = "ami-003e7cf4a21db7053"
+  ami           = "ami-0b6d9d3d33ba97d99"
   instance_type = "t3.micro"  # adjust as needed
 
   tags = {
-    Name = "Windows-EKS-Optimized-Server"
+    Name = "Ubuntu Server 26.04 LTS"
   }
 }
